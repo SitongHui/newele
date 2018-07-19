6bf5c4cd495fa78ced98df3c1b00763b69e54685
SitongHui <492150523@qq.com>
2018-07-19 15:43:37 +0800

    更新“

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
 window.onload=function(){
-        var search=document.getElementsByTagName("input")
+        var search=document.getElementsByTagName("input")[0];
         search.onclick=function(){
             alert(1);
-            // alert(input.style.width);
-        }
-        var body=document.getElementsByTagName("body");
-        body.onclick=function(){
-            alert(1);
+            console.log(search.style.width);
         }
         var lilist=document.getElementsByClassName("ul_li");
         lilist.onmouseenter=function(){
